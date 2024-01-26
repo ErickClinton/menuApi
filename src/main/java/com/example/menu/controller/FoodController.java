a3ba68da807ad6943fa266641117f64e50a00b9f
Erick Clinton <erickleite2009321@gmail.com>
2024-01-26 17:05:02 -0300

    Basic project structure and listing and inclusion routes

---
+++ src/main/java/com/example/menu/controller/FoodController.java
@@ -0,0 +1,32 @@
+package com.example.menu.controller;
+
+import com.example.menu.food.Food;
+import com.example.menu.food.FoodRepository;
+import com.example.menu.food.FoodRequestDto;
+import com.example.menu.food.FoodResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowCredentials = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDto data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDto> getAll(){
+        List<FoodResponseDto> foodList = repository.findAll().stream().map(FoodResponseDto:: new).toList();
+        return foodList;
+    }
+}
